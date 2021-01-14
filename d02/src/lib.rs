5710ae631d740c37e69e6891a5d27e1db6e93ec2
Kristjan Siimson <k.siimson@sportradar.com>
2021-01-14 11:22:17 +0100

    d02 - operate on bytes directly for extra perf

--- d02/src/lib.rs
+++ d02/src/lib.rs
@@ -1,142 +1,157 @@
+extern crate btoi;
+
+use btoi::btoi;
 use std::io::BufRead;
-use std::str::FromStr;
-use std::string::ParseError;
 
-#[derive(Debug)]
-pub struct P1InputLine {
-    valid: bool,
-}
+fn p1_validate(line: &[u8]) -> bool {
+    let mut iter = &mut line.iter();
 
-impl FromStr for P1InputLine {
-    type Err = ParseError;
+    let mut buffer = [0; 32];
+    let mut buffer_len = 0;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut iter = s.chars();
-        let mut buffer = String::new();
-        for c in &mut iter {
-            if c == '-' {
-                break;
-            }
-            buffer.push(c);
+    for c in &mut iter {
+        if c == &b'-' {
+            break;
         }
-        let min: usize = buffer.parse().expect("Min characters");
-        buffer.clear();
-        for c in &mut iter {
-            if c == ' ' {
-                break;
-            }
-            buffer.push(c);
-        }
-        let max: usize = buffer.parse().expect("Max characters");
-        buffer.clear();
-        for c in &mut iter {
-            if c == ':' {
-                iter.next();
-                break;
-            }
-            buffer.push(c);
-        }
-        let expected_character: char = buffer.parse().expect("Character");
-        buffer.clear();
-
-        let mut occurences = 0;
-        for c in &mut iter {
-            if c == expected_character {
-                occurences += 1;
-            }
-        }
-
-        Ok(P1InputLine {
-            valid: (occurences >= min) && (occurences <= max),
-        })
+        buffer[buffer_len] = *c;
+        buffer_len += 1;
     }
-}
 
-pub struct P2InputLine {
-    valid: bool,
-}
+    let min = btoi(&buffer[..buffer_len]).expect("minimum number of occurences");
 
-impl FromStr for P2InputLine {
-    type Err = ParseError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut iter = s.chars();
-        let mut buffer = String::new();
-        for c in &mut iter {
-            if c == '-' {
-                break;
-            }
-            buffer.push(c);
+    buffer_len = 0;
+    for c in &mut iter {
+        if c == &b' ' {
+            break;
         }
-        let pos1: usize = buffer.parse().expect("Min characters");
-        buffer.clear();
-        for c in &mut iter {
-            if c == ' ' {
-                break;
-            }
-            buffer.push(c);
-        }
-        let pos2: usize = buffer.parse().expect("Max characters");
-        buffer.clear();
-        for c in &mut iter {
-            if c == ':' {
-                iter.next();
-                break;
-            }
-            buffer.push(c);
-        }
-        let expected_character: char = buffer.parse().expect("Character");
-        buffer.clear();
-
-        let mut pos = 1;
-        let mut valid = false;
-        for c in &mut iter {
-            if (pos == pos1) || (pos == pos2) {
-                valid ^= c == expected_character;
-            }
-            pos += 1;
-        }
-
-        Ok(P2InputLine { valid })
+        buffer[buffer_len] = *c;
+        buffer_len += 1;
     }
+
+    let max = btoi(&buffer[..buffer_len]).expect("maximum number of occurences");
+
+    let expected_byte = iter.next().expect("expected character");
+
+    iter.next(); // ':'
+    iter.next(); // ' '
+
+    let mut occurences = 0;
+
+    for c in &mut iter {
+        if c == expected_byte {
+            occurences += 1;
+        }
+    }
+
+    occurences >= min && occurences <= max
 }
 
 pub fn p1_parse_and_solve<R>(mut reader: R) -> usize
 where
     R: BufRead,
 {
-    let mut input = String::new();
-    let mut c: usize = 0;
-    while let Ok(n) = reader.read_line(&mut input) {
-        if n == 0 {
+    let mut page = [0; 4096];
+    let mut buffer = [0; 32];
+    let mut buffer_len = 0;
+
+    let mut valid_count = 0;
+
+    while let Ok(page_len) = reader.read(&mut page) {
+        if page_len == 0 {
             break;
         }
-        if let Ok(line) = input.trim().parse::<P1InputLine>() {
-            if line.valid {
-                c += 1;
+
+        for c in &page[..page_len] {
+            if c == &b'\n' {
+                valid_count += p1_validate(&buffer[..buffer_len]) as usize;
+                buffer_len = 0;
+            } else {
+                buffer[buffer_len] = *c;
+                buffer_len += 1;
             }
         }
-        input.clear();
     }
-    c
+
+    if buffer_len > 0 {
+        valid_count += p1_validate(&buffer[..buffer_len]) as usize;
+    }
+
+    valid_count
+}
+
+fn p2_validate(line: &[u8]) -> bool {
+    let mut iter = &mut line.iter();
+
+    let mut buffer = [0; 32];
+    let mut buffer_len = 0;
+
+    for c in &mut iter {
+        if c == &b'-' {
+            break;
+        }
+        buffer[buffer_len] = *c;
+        buffer_len += 1;
+    }
+
+    let pos_first = btoi::<usize>(&buffer[..buffer_len]).expect("minimum number of occurences");
+
+    buffer_len = 0;
+    for c in &mut iter {
+        if c == &b' ' {
+            break;
+        }
+        buffer[buffer_len] = *c;
+        buffer_len += 1;
+    }
+
+    let pos_second = btoi::<usize>(&buffer[..buffer_len]).expect("maximum number of occurences");
+
+    let expected_byte = iter.next().expect("expected character");
+
+    iter.next(); // ':'
+    iter.next(); // ' '
+
+    let mut valid = false;
+    let mut pos = 1;
+    for c in iter {
+        if (pos == pos_first) || (pos == pos_second) {
+            valid ^= c == expected_byte;
+        }
+        pos += 1;
+    }
+
+    valid
 }
 
 pub fn p2_parse_and_solve<R>(mut reader: R) -> usize
-where
-    R: BufRead,
+    where
+        R: BufRead,
 {
-    let mut input = String::new();
-    let mut c: usize = 0;
-    while let Ok(n) = reader.read_line(&mut input) {
-        if n == 0 {
+    let mut page = [0; 4096];
+    let mut buffer = [0; 32];
+    let mut buffer_len = 0;
+
+    let mut valid_count = 0;
+
+    while let Ok(page_len) = reader.read(&mut page) {
+        if page_len == 0 {
             break;
         }
-        if let Ok(line) = input.trim().parse::<P2InputLine>() {
-            if line.valid {
-                c += 1;
+
+        for c in &page[..page_len] {
+            if c == &b'\n' {
+                valid_count += p2_validate(&buffer[..buffer_len]) as usize;
+                buffer_len = 0;
+            } else {
+                buffer[buffer_len] = *c;
+                buffer_len += 1;
             }
         }
-        input.clear();
     }
-    c
+
+    if buffer_len > 0 {
+        valid_count += p2_validate(&buffer[..buffer_len]) as usize;
+    }
+
+    valid_count
 }
